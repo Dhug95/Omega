f07c0d41404563a4da61093d563469934b3c9171
Francesco D'Abbraccio <dabbraccio.francesco@gmail.com>
2017-08-19 22:48:32 +0200

    Aggiunto avatar per gli utenti

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ApplicationRecord
 
   has_many :properties
 
-   validates :username, uniqueness: true
+  validates :username, uniqueness: true
+
+  has_attached_file :avatar, styles: { small: "200x200>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
