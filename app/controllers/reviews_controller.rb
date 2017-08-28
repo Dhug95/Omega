9de859bef0460cdd71d86d206a1a0671f70098e5
Luca Faraoni <15liuk15@gmail.com>
2017-08-28 21:18:53 +0000

    Terminato HomeSection

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
   def create
-   @property = Property.find(params[:property_id])
-   @review = @property.reviews.create(reviews_params)
-   current_user.reviews << @review
-   redirect_to property_path(@property)
- end
+    @property = Property.find(params[:property_id])
+    @review = @property.reviews.create(reviews_params)
+    current_user.reviews << @review
+    redirect_to property_path(@property)
+  end
 
- def destroy
+  def destroy
     @property = Property.find(params[:property_id])
     @review = @property.reviews.find(params[:id])
     @review.destroy
