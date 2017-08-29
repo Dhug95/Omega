8e94e72274e4b1f3c4d853d30ac7cf0ed1492ecd
CarmineMansueto95 <carmine@localhost.localdomain>
2017-08-29 22:01:55 +0000

    Aggiunta funzionalità Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#User.create!([{email: 'admin@omega.it'}, {encrypted_password: '1q2w3e4r5t6y7u8i9o0p'}, {password: 'SeCrEt'}, {password_confirmation: 'SeCrEt'}, {username: 'admin'}, {name: 'admin'}, {admin: 'true'}])
+user = User.new(
+  :email => "admin@omega.it",
+  :password => "SeCrEt",
+  :password_confirmation => "SeCrEt",
+  :username => "admin",
+  :name => "admin",
+  :admin => "true"
+)
+#user.skip_confirmation!
+user.save!
