dbb3b1c38f00ee304b2a5ab5f1a84d6278d4c283
CarmineMansueto95 <carmine@localhost.localdomain>
2017-08-31 09:31:18 +0000

    Aggiunte validation alle proprietà

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 50}
+  validates :description, presence:true, length: { minimum: 10}
+  validates :price, presence:true, numericality: {greater_than: 0}
+  validates :city, presence: true
+
   belongs_to :user
   has_and_belongs_to_many :favouriters, class_name: "User", join_table: "favouriters_and_favourites"
   paginates_per 3
