b7b5edb9525ff5908e5b764aad12043791f55f1f
CarmineMansueto95 <carmine@localhost.localdomain>
2017-09-01 12:09:44 +0000

    Aggiunti test Rspec sulle recensioni

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
-  validates :valutation, presence: true
+  validates :valutation, presence: true, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 5}
 end
